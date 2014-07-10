b4ac4e73562ac4808e41965ec81bc2311927707a
Dave Powers <djpowers89@gmail.com>
2014-07-09 22:41:00 -0400

    Move rumor class to separate file under models directory

--- app.rb
+++ app.rb
@@ -25,24 +25,6 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
   require file
 end
 
-class Rumor < ActiveRecord::Base
-  validates :body, presence: true, length: { minimum: 5 }
-  validates :submitter, length: { minimum: 2 }, allow_blank: true
-
-  @client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV['YOUR_CONSUMER_KEY']
-    config.consumer_secret = ENV['YOUR_CONSUMER_SECRET']
-  end
-
-  def self.add_twitter_rumors
-    @client.search('#patkellyrumors -rt', result_type: 'recent').take(3).collect do |tweet|
-      Rumor.find_or_create_by(body: tweet.text, submitter: tweet.user.screen_name) do |rumor|
-        rumor.tweet_id = tweet.id
-      end
-    end
-  end
-end
-
 helpers do
   def csrf_token
     Rack::Csrf.csrf_token(env)
