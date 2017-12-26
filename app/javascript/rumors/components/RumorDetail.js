ab8f26941744a1647b19ceea3246eab341f71e37
Dave Powers <djpowers89@gmail.com>
2017-12-26 14:58:48 -0500

    Style tweaks on display of rumor submitter

--- app/javascript/rumors/components/RumorDetail.js
+++ app/javascript/rumors/components/RumorDetail.js
@@ -3,10 +3,11 @@ import React from 'react';
 class RumorDetail extends React.Component {
   render() {
     return (
-      <div>
-        <p>{this.props.body}</p>
-        <span>Submitted by {this.props.submitter} at {this.props.posted_time}</span>
-      </div>
+      <p>
+        &quot;{this.props.body}&quot;
+        <br />
+        <span className="submitted">Submitted by:</span> {this.props.submitter ? this.props.submitter : <span className="anonymous">Anonymous</span>} at {this.props.posted_time}
+      </p>
     );
   }
 }
