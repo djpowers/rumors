27888ae2188aa91ec757c504033e09530486c028
Dave Powers <djpowers89@gmail.com>
2017-12-26 15:20:49 -0500

    Style rumor detail

--- app/javascript/rumors/components/RumorDetail.js
+++ app/javascript/rumors/components/RumorDetail.js
@@ -3,7 +3,7 @@ import React from 'react';
 class RumorDetail extends React.Component {
   render() {
     return (
-      <p>
+      <p className="rumorDetail">
         &quot;{this.props.body}&quot;
         <br />
         <span className="submitted">Submitted by:</span> {this.props.submitter ? this.props.submitter : <span className="anonymous">Anonymous</span>} at {this.props.posted_time}
